0c15e89fd9f3c0a606f931e9d326ad83d9a90d02
PaulaLebedev <55434232+PaulaLebedev@users.noreply.github.com>
2020-02-28 15:16:50 +0200

    Create app.js

---
+++ api2/app.js
@@ -0,0 +1,14 @@
+fetch('https://api.magicthegathering.io/v1/cards')
+ .then((responce) => {
+     return responce.json();
+ })
+ .then((data) => {
+    console.log(data.cards);
+    const container = document.getElementById('container')
+    data.cards.forEach(card => {
+        console.log(card) 
+        let cardDiv = document.createElement('div')
+        cardDiv.innerHTML = '<div class="name">' + card.name + '</div> <div class="manaCost">' + card.manaCost + '</div>'
+        container.append(cardDiv)
+    });
+ });
